e156cdffa82b16c265fb8f89d038f293fb5c751f
Conero <brximl@163.com>
2017-05-30 17:45:02 +0800

    no commit message

--- src/Jess/jess.ts
+++ src/Jess/jess.ts
@@ -1,9 +1,9 @@
 /**
  * @start 2017年5月26日 星期五
  * @name storage处理
- * @version 0.0.1
+ * @version 0.0.2
  * @author Joshua Coenro
- * @date 20170526
+ * @date 20170527
  * @other tsc 学习与实践
  */
 class Jess{
@@ -12,19 +12,28 @@ class Jess{
     engine: string; // session/local
     table:string;
     private tableJson:object;
-
+    /**
+     * 构造函数
+     * @param name 作用域
+     * @param sengine 引擎名称
+     */
     constructor(public name, public sengine){
-        this.version = "0.0.1";
+        this.version = "0.0.2";
         this.descrip = "SessionStorage 和 LocalStorage 对象 v-k数据库";
         this.engine = sengine? sengine:'session';
         if(name) this.table;
         this.tableJson = this.getJson();
     }
-    // 设置表名或作用名
+    /**
+     * 设置表名或作用名
+     * @param name string 设置作用名
+     */
     setName(name){
         this.table = name;
     }
-    // 获取 json 值
+    /**
+     * 获取作用域下的所有的json数据
+     */
     getJson(){
         var content = "";        
         if(this.engine == 'local') content = localStorage.getItem(this.table);
@@ -32,28 +41,54 @@ class Jess{
         if(content) this.tableJson = JSON.parse(content);
         return this.tableJson;
     }
-    // 获取 json 值
+    /**
+     * 获取 key键值下的值
+     * @param key string
+     * @return mixed
+     */
     getVal(key){
         return this.tableJson[key]? this.tableJson[key]:this.tableJson[key];
     }
-    // 键值是否存在
+    /**
+     * 检查键值是否存在
+     * @param key string
+     * @return bool
+     */
     hasKey(key){
         return this.tableJson[key]? true:false;
     }
+    /**
+     * 设置当前的值
+     * @param key 
+     * @param value 
+     */
     setVal(key,value){
         this.tableJson[key] = value;
         this.save(this.tableJson);
     }
+    /**
+     * 通过键名删除值
+     * @param key string
+     */
     delVal(key){
         if(this.tableJson[key]){
             delete(this.tableJson[key]);
             this.save(this.tableJson);
         }
     }
+    /**
+     * 保存json值到storage栈内
+     * @param data {}
+     */
     save(data:object){
         if(this.engine == 'local') localStorage.setItem(this.table,JSON.stringify(data));
         else sessionStorage.setItem(this.table,JSON.stringify(data));
     }
+    /**
+     * 设置获取于删除值
+     * @param key string
+     * @param value minxed
+     */
     value(key,value){
         if(typeof(value) == 'undefined'){
             return this.delVal(key);
